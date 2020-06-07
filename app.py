4e4a32ec5806dda7c61e7633de09904550af674d
Silvio <silviolima@gmail.com>
2020-06-07 00:18:59 -0300

    TextBlob e Spacy

--- app.py
+++ app.py
@@ -135,7 +135,7 @@ def process_text(model_name, text):
 SPACY_MODEL_NAMES = ["pt_core_news_sm"]
 camoes = "Amor é fogo que arde sem se ver. Poema escrito por Luís Vaz de Camões. Começa assim. Amor é um fogo que arde sem se ver. É ferida que dói, e não se sente. É um contentamento descontente. É dor que desatina sem doer. É um não querer mais que bem querer. É um andar solitário entre a gente. É nunca contentar-se e contente. É um cuidar que ganha em se perder. É querer estar preso por vontade. É servir a quem vence, o vencedor. É ter com quem nos mata, lealdade. Mas como causar pode seu favor. Nos corações humanos amizade. Se tão contrário a si é o mesmo Amor?"
 
-armstrong = "What A Wonderful World, Louis Armstrong. The Louis Armstrong Songbook. Listening. I see trees of green, Red roses too, I see them bloom, For me and you, And I think to myself, What a wonderful world. I see skies of blue and clouds of white, The bright blessed day, The dark sacred night, And I think to myself, What a wonderful world. The colors of the rainbow, So pretty in the sky, Are also on the faces, Of people going by, I see friends shaking hands, Saying: How do you do? They're really saying, I love you."
+armstrong = "What A Wonderful World, Louis Armstrong. The Louis Armstrong Songbook Listening. I see trees of green, Red roses too, I see them bloom, For me and you, And I think to myself, What a wonderful world. I see skies of blue and clouds of white, The bright blessed day, The dark sacred night, And I think to myself, What a wonderful world. The colors of the rainbow, So pretty in the sky, Are also on the faces, Of people going by, I see friends shaking hands, Saying: How do you do? They're really saying, I love you."
 
 
 HTML_WRAPPER = """<div style="overflow-x: auto; border: 1px solid #e6e9ef; border-radius: 0.25rem; padding: 1rem; margin-bottom: 2.5rem">{}</div>"""
@@ -165,6 +165,7 @@ def main():
     dict_idioma_full = lista_idiomas_full()
     flag = 'carregar'
     warning = "Carregue um arquivo ou clique num arquivo da demonstração."
+    warning_spacy = "Textos em Português."
     carregado = "Arquivo demonstraçao carregado..."
     uploaded = "Arquivo uploaded"
 
@@ -176,7 +177,6 @@ def main():
         
     if choice == 'Spacy':
         try:
-            #file = carregar_texto('txt')
             doc, flag = carregar_texto('txt')
             if flag == "demo":
                 st.success(carregado)
@@ -186,76 +186,76 @@ def main():
             idioma_original = get_value(blob.detect_language(),dict_idioma_full)   
             st.markdown("### Texto")
             st.write(doc)
-          
-            doc = process_text('pt_core_news_sm',doc)
-            nlp = spacy.load('pt_core_news_sm')
-            st.text([(word, word.ent_type_) for word in doc if word.ent_type_])
-            displacy.render(doc, style='ent', jupyter=True)
+            if idioma_original == "Portuguese":
+                doc = process_text('pt_core_news_sm',doc)
+                nlp = spacy.load('pt_core_news_sm')
+                st.text([(word, word.ent_type_) for word in doc if word.ent_type_])
+                displacy.render(doc, style='ent', jupyter=True)
         
-            if "parser" in nlp.pipe_names:
-                st.header("Dependency Parse & Part-of-speech tags")
-                st.sidebar.header("Dependency Parse")
-                split_sents = st.sidebar.checkbox("Split sentences", value=True)
-                collapse_punct = st.sidebar.checkbox("Collapse punctuation", value=True)
-                collapse_phrases = st.sidebar.checkbox("Collapse phrases")
-                compact = st.sidebar.checkbox("Compact mode")
-                options = {
+                if "parser" in nlp.pipe_names:
+                    st.header("Dependency Parse & Part-of-speech tags")
+                    st.sidebar.header("Dependency Parse")
+                    split_sents = st.sidebar.checkbox("Split sentences", value=True)
+                    collapse_punct = st.sidebar.checkbox("Collapse punctuation", value=True)
+                    collapse_phrases = st.sidebar.checkbox("Collapse phrases")
+                    compact = st.sidebar.checkbox("Compact mode")
+                    options = {
                        "collapse_punct": collapse_punct,
                        "collapse_phrases": collapse_phrases,
                        "compact": compact,
                        }
-                docs = [span.as_doc() for span in doc.sents] if split_sents else [doc]
-                for sent in docs:
-                    html = displacy.render(sent, options=options)
-                    # Double newlines seem to mess with the rendering
-                    html = html.replace("\n\n", "\n")
-                    if split_sents and len(docs) > 1:
-                        st.markdown(f"> {sent.text}")
+                    docs = [span.as_doc() for span in doc.sents] if split_sents else [doc]
+                    for sent in docs:
+                        html = displacy.render(sent, options=options)
+                        # Double newlines seem to mess with the rendering
+                        html = html.replace("\n\n", "\n")
+                        if split_sents and len(docs) > 1:
+                            st.markdown(f"> {sent.text}")
+                        st.write(HTML_WRAPPER.format(html), unsafe_allow_html=True)
+
+                if "ner" in nlp.pipe_names:
+                    st.header("Named Entities")
+                    st.sidebar.header("Named Entities")
+                    default_labels = ["PER", "LOC", "ORG", "MISC"]
+                    labels = st.sidebar.multiselect("Entity labels", nlp.get_pipe("ner").labels, default_labels)
+                    html = displacy.render(doc, style="ent", options={"ents": labels})
+
+                    # Newlines seem to mess with the rendering
+                    html = html.replace("\n", " ")
                     st.write(HTML_WRAPPER.format(html), unsafe_allow_html=True)
-
-            if "ner" in nlp.pipe_names:
-                st.header("Named Entities")
-                st.sidebar.header("Named Entities")
-                default_labels = ["PER", "LOC", "ORG", "MISC"]
-                labels = st.sidebar.multiselect("Entity labels", nlp.get_pipe("ner").labels, default_labels)
-                html = displacy.render(doc, style="ent", options={"ents": labels})
-
-                # Newlines seem to mess with the rendering
-                html = html.replace("\n", " ")
-                st.write(HTML_WRAPPER.format(html), unsafe_allow_html=True)
-                attrs = ["text", "label_", "start", "end", "start_char", "end_char"]
-                if "entity_linker" in nlp.pipe_names:
-                    attrs.append("kb_id_")
-                data = [[str(getattr(ent, attr)) for attr in attrs]
-                for ent in doc.ents
-                if ent.label_ in labels]
-                df = pd.DataFrame(data, columns=attrs)
-                st.dataframe(df)
+                    attrs = ["text", "label_", "start", "end", "start_char", "end_char"]
+                    if "entity_linker" in nlp.pipe_names:
+                        attrs.append("kb_id_")
+                    data = [[str(getattr(ent, attr)) for attr in attrs]
+                    for ent in doc.ents
+                    if ent.label_ in labels]
+                    df = pd.DataFrame(data, columns=attrs)
+                    st.dataframe(df)
     
-            if "textcat" in nlp.pipe_names:
-                st.header("Text Classification")
-                st.markdown(f"> {text}")
-                df = pd.DataFrame(doc.cats.items(), columns=("Label", "Score"))
-                st.dataframe(df)
+                if "textcat" in nlp.pipe_names:
+                    st.header("Text Classification")
+                    st.markdown(f"> {text}")
+                    df = pd.DataFrame(doc.cats.items(), columns=("Label", "Score"))
+                    st.dataframe(df)
             
-            vector_size = nlp.meta.get("vectors", {}).get("width", 0)
-            if vector_size:
-                st.header("Vectors & Similarity")
-                st.code(nlp.meta["vectors"])
-                text1 = st.text_input("Text or word 1", "apple")
-                text2 = st.text_input("Text or word 2", "orange")
-                doc1 = process_text(spacy_model, text1)
-                doc2 = process_text(spacy_model, text2)
-                similarity = doc1.similarity(doc2)
-                if similarity > 0.5:
-                    st.success(similarity)
-                else:
-                    st.error(similarity)
+                vector_size = nlp.meta.get("vectors", {}).get("width", 0)
+                if vector_size:
+                    st.header("Vectors & Similarity")
+                    st.code(nlp.meta["vectors"])
+                    text1 = st.text_input("Text or word 1", "apple")
+                    text2 = st.text_input("Text or word 2", "orange")
+                    doc1 = process_text(spacy_model, text1)
+                    doc2 = process_text(spacy_model, text2)
+                    similarity = doc1.similarity(doc2)
+                    if similarity > 0.5:
+                        st.success(similarity)
+                    else:
+                        st.error(similarity)
 
-            st.header("Token attributes")
+                st.header("Token attributes")
 
-            if st.button("Show token attributes"):
-                attrs = [
+                if st.button("Show token attributes"):
+                    attrs = [
         "idx",
         "text",
         "lemma_",
@@ -272,20 +272,26 @@ def main():
         "is_punct",
         "like_num",
     ]
-                data = [[str(getattr(token, attr)) for attr in attrs] for token in doc]
-                df = pd.DataFrame(data, columns=attrs)
-                st.dataframe(df)
+                    data = [[str(getattr(token, attr)) for attr in attrs] for token in doc]
+                    df = pd.DataFrame(data, columns=attrs)
+                    st.dataframe(df)
 
 
-            st.header("JSON Doc")
-            if st.button("Show JSON Doc"):
-                st.json(doc.to_json())
+                st.header("JSON Doc")
+                if st.button("Show JSON Doc"):
+                    st.json(doc.to_json())
 
-            st.header("JSON model meta")
-            if st.button("Show JSON model meta"):
-                st.json(nlp.meta)
+                st.header("JSON model meta")
+                if st.button("Show JSON model meta"):
+                    st.json(nlp.meta)
+
+            if idioma_original != "Portuguese":
+                st.erro("Apenas textos em Portugues, por favor")
+
+
         except:
-            st.warning(choice + " --> "+warning)
+                st.info(choice + " --> "+warning_spacy)
+                st.warning(choice + " --> "+warning)
         
 
         
@@ -317,7 +323,8 @@ def main():
         st.subheader("I hope you enjoy it and use to learn something")
         st.subheader("For while only txt files")
         st.subheader("Built with Streamlit, Textblob and Spacy")
-        st.write("Problems:")
+        st.write("Obs: Spacy loaded with model pt_core_news_sm")
+        st.write("Problems/bugs:")
         st.write(" - sometimes the original language can't be correctly detected")
         st.write(" - sometimes the sound will fail.")
         st.subheader("Powerful example of Spacy code analyse")
